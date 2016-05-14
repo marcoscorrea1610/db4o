719180d8b71b9e7bd3f5bd6bdca771cf9b86b531
PC <PC@PC-PC>
2016-05-14 17:21:00 -0400

    primer commit

---
+++ src/db4o_congreso/DB4O_Congreso.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db4o_congreso;
+
+//import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author hugo
+ */
+public class DB4O_Congreso {
+   
+    //table = new JTable(modelo);
+       
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frame1 frm=new frame1();
+        frm.setVisible(true);
+        // TODO code application logic here
+        //es necesario realizar los 2 imports anteriores, ya que si no es ponible que automaticamente no los haga produciendose un error.
+   
+    //creamos un ponente
+     
+    
+    }
+    
+}
+
